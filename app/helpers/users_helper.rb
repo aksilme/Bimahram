eb3a66c97bb79c7aee1c453f085ec3481ef0df4e
koceila <lounas.koceila@gmail.com>
2017-03-22 23:10:48 +0100

    add friendships status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def action_buttons(user)
+    case current_user.friendship_status(user) when "friends"
+      "Supprimer la demande"
+      when "pending"
+         "annuler la demande"
+      when "requested"
+         "Accepter"
+      when "not_friends"
+        "Envoyer une demande"
+    end
+  end
+
 end
