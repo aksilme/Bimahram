c3d38623a66b80e4fea35d9ce0b7d2e30c81bb6a
koceila <lounas.koceila@gmail.com>
2017-04-07 17:46:00 +0200

    change cast birthday

--- db/migrate/20170311225602_change_birhday_to_integer_in_user.rb
+++ db/migrate/20170311225602_change_birhday_to_integer_in_user.rb
@@ -1,5 +1,5 @@
 class ChangeBirhdayToIntegerInUser < ActiveRecord::Migration[5.0]
   def change
-    change_column :users, :birthday, :integer
+    change_column :users, :birthday, 'integer USING CAST(birthday AS integer)'
   end
 end
