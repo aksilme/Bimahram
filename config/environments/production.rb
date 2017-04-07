32cc08efe1b4586af354b9d73b993bf289845ede
koceila <lounas.koceila@gmail.com>
2017-04-08 01:39:31 +0200

    fixing prod email errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'bimahram.herokuapp.com' }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  Rails.application.routes.default_url_options[:host] = 'bimahram.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       port: ENV['MAILGUN_SMTP_PORT'],
